be6821deedef17dab2611d32c28d199000a36b1d
mbrodt <madsbrodt3@gmail.com>
2023-03-17 00:01:32 +0100

    Log

--- pages/api/gpt.ts
+++ pages/api/gpt.ts
@@ -86,6 +86,11 @@ export default async function handler(
 
     console.log("INSIDE POST", req.body.text);
 
+    console.log(
+      "IN POST process.env.OPENAI_API_KEY",
+      process.env.OPENAI_API_KEY
+    );
+
     const responseText = await generateResponse(userText);
 
     console.log("RESPONSE", responseText);
