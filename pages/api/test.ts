90adf0eb563c78f06e94e2a79ff2c9c72fe20a56
mbrodt <madsbrodt3@gmail.com>
2023-03-19 00:36:15 +0100

    Test new Edge function logic

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,15 +1,103 @@
 import { NextResponse } from "next/server";
-import type { NextFetchEvent, NextRequest } from "next/server";
+import type { NextRequest } from "next/server";
+// import { Configuration, OpenAIApi } from "openai";
+import cors from "edge-cors";
 
 export const config = {
   runtime: "edge",
 };
 
-export default function MyEdgeFunction(
-  request: NextRequest,
-  context: NextFetchEvent
-) {
-  return NextResponse.json({
-    name: `Hello, from ${request.url} I'm an Edge Function!`,
-  });
+console.log("process.env.OPENAI_API_KEY", process.env.OPENAI_API_KEY);
+console.log("process.env.OPENAI_ORGANIZATION", process.env.OPENAI_ORGANIZATION);
+
+const generateResponse = async (prompt: string) => {
+  console.log("prompt", prompt);
+
+  try {
+    const gptResponse = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "system",
+              content:
+                "You are a helpful assistant. You will answer any questions the user asks or do whatever they say. You will ALWAYS respond in the same language as the user. This will likely be EITHER Danish or English",
+            },
+            { role: "user", content: prompt },
+          ],
+        }),
+      }
+    );
+    const gptData = await gptResponse.json();
+
+    const responseText = gptData.choices[0].message.content;
+    return responseText;
+  } catch (error) {
+    console.log("error message", error.message);
+  }
+};
+
+export default async function MyEdgeFunction(request: NextRequest) {
+  if (request.method === "GET") {
+    return cors(
+      request,
+      NextResponse.json(
+        'This endpoint only accepts POST requests. Try sending a POST request to "/api/gpt" instead.'
+      )
+    );
+  }
+
+  if (request.method === "POST") {
+    // const body = await request.json();
+
+    const body = await request.formData();
+    console.log("body:", body);
+
+    const userText = body.get("text") as string;
+    console.log("userText:", userText);
+    const responseUrl = body.get("response_url") as string;
+    console.log("responseUrl:", responseUrl);
+
+    // res.json({response_type: 'in_channel', text:"Request received! Generating response..."});
+    // res.json({
+    //   text: `\n>${userText}.\n *Genererer svar...*`,
+    // });
+
+    console.log(
+      "IN POST process.env.OPENAI_API_KEY",
+      process.env.OPENAI_API_KEY
+    );
+
+    // const responseText = await generateResponse(userText);
+    generateResponse(userText).then((responseText) => {
+      console.log("RESPONSE TEXT", responseText);
+
+      console.log("posting to responseUrl", responseUrl);
+      fetch(responseUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: responseText,
+        }),
+      }).then((res) => {
+        console.log("DONE POSTING TO SLACK", res);
+      });
+    });
+
+    return cors(
+      request,
+      NextResponse.json({
+        text: `*${userText}*\n_Genererer svar..._`,
+      })
+    );
+  }
 }
